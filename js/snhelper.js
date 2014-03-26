c03fd83d96d4c623616291d54577db899c069f5e
amitrogye <amit.rogye@fruitionpartners.com>
2014-03-26 15:53:52 -0500

    new 1

--- js/snhelper.js
+++ js/snhelper.js
@@ -12,10 +12,10 @@ window.bookmarklet({
 
 
         //alert("hello -- " + jQuery.fn.jquery);
-
+javascript:(function(){window.open($('gsft_main').contentWindow.location.href);})();
         var flg = false;
         if (jQuery("#ferromenu-controller-0").length <= 0) {
-            var wheel = '<ul id="nav"><li><a href="javascript:void(0);">UP</a></li><li><a href="javascript:void(0);">New</a></li></ul>';
+            var wheel = '<ul id="nav"><li><a href="javascript:void(0);">UP</a></li><li><a href="javascript:void(0);console.log($(\'gsft_main\'))">New</a></li></ul>';
             jQuery("body").append(wheel);
             jQuery(document).ready(function() {
                 jQuery("#nav").ferroMenu();
